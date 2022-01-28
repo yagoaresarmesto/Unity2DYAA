de377bedd950685cf8a8a7f3189c62555ee4ee44
yagoaresarmesto <yagofingoi1234@gmail.com>
2022-01-28 12:14:37 +0100

    Añadiendo animaciones de personaje sin moverse
    Añado la animaciones cuando el sprite esté estático,  que siga con la animación de movimiento cuando el sprite no se mueve.

--- PracticaUnityYAA/Assets/Scripts/PersonajeAnimaciones.cs
+++ PracticaUnityYAA/Assets/Scripts/PersonajeAnimaciones.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 
 public class PersonajeAnimaciones : MonoBehaviour
 {
+    [SerializeField] private string layerIdle;
+
+    [SerializeField] private string layerCaminar;
     private Animator _animator;
     private PersonajeMovimiento _personajeMovimiento;
 
     private readonly int direccionX = Animator.StringToHash("x");
     private readonly int direccionY = Animator.StringToHash("y");
+
     private void Awake()
     {
         _animator = GetComponent<Animator>();
@@ -17,6 +21,8 @@ public class PersonajeAnimaciones : MonoBehaviour
 
     private void Update()
     {
+        ActualizarLayers();
+
         if (_personajeMovimiento.EnMovimiento == false) //Si nuestro personaje no se mueve
         {
             return;
@@ -25,5 +31,28 @@ public class PersonajeAnimaciones : MonoBehaviour
         _animator.SetFloat(direccionX, _personajeMovimiento.DireccionMovimiento.x);
         _animator.SetFloat(direccionY, _personajeMovimiento.DireccionMovimiento.y);
     }
+
+    private void ActivarLayer(string nombreLayer)
+    {
+        for (int i = 0; i < _animator.layerCount; i++) //Antes de usar un layer hay que desactuvarlo
+        {
+            _animator.SetLayerWeight(i, 0); //Pone un peso al layer 0 descativado, 1 activado
+        }
+
+        _animator.SetLayerWeight(_animator.GetLayerIndex(nombreLayer), 1);
+    }
+
+    private void ActualizarLayers() //Actualizar el layer en base a su movimiento
+    {
+        if (_personajeMovimiento.EnMovimiento)
+        {
+            ActivarLayer(layerCaminar);
+        }
+        else
+        {
+            ActivarLayer(layerIdle);
+        }
+    }
+
 }
 
